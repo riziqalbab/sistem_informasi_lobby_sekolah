a65bfd4068bc335c3b04e8addcbfdb268388c4ff
riziqalbab <albabriziq36@gmail.com>
2024-10-21 16:14:48 +0700

    logo smk

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,6 +1,6 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
-import { PropsWithChildren } from 'react';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link } from "@inertiajs/react";
+import { PropsWithChildren } from "react";
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
@@ -10,7 +10,7 @@ export default function Guest({ children }: PropsWithChildren) {
                     <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
                 </Link>
             </div>
-
+            <h1 className="font-bold text-2xl m-2">LOBBY SMK NEGERI 1 KEBUMEN</h1>
             <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {children}
             </div>
